06bf55eb061c604aa66cbbf8100199cd94095d6b
Wexoo <p.weixlbaumer@gmail.com>
2022-10-07 14:39:43 +0200

    4 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component, VERSION } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  VERSION,
+} from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit, AfterViewInit {
+  
+  private toggleEvent;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    this.toggleEvent = this.createToggleEvent();
+    this.getFaqHeaders().forEach((c) =>
+      c.addEventListener('click', this.toggleEvent)
+    );
+  }
+
+  private createToggleEvent() {
+    return this.toggleFaq.bind(this);
+  }
+
+  toggleFaq(event) {
+    this.closeOpenNodes(event);
+    event.target.parentNode.classList.toggle('header-red');
+    // this.getFaqHeaders().removeEventListener('click', this.toggleEvent);
+  }
+
+  private closeOpenNodes(event) {
+    this.getFaqHeaders().forEach((c) => {
+      if (c != event.target) {
+        c.parentNode.classList.remove('header-red');
+      }
+    });
+  }
+
+  // ngOnDestroy(): void {
+  // 	console.log('on destroy called!');
+  // 	this.getFaqHeaders().removeEventListener('click', this.createToggleEvent());
+  // }
+
+  private getFaqHeaders() {
+    return this.elementRef.nativeElement.querySelectorAll('.faq-container>h2');
+  }
 }
